a0efb3622cf58db472b7b1cccdc2722072f14e2e
SPTC-NTIC Lenix Eduardo Rotundo de Camargo <lenix.lerc@sptc.policiacientifica.net>
2021-08-27 08:41:18 -0300

    Correções
    Correções feitas.

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 var express = require('express');
 const cabideController = require('../controllers/cabideController');
 var router = express.Router();
-const servicosController = require('../controllers/cabideController')
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
-});
-router.post('/entrar', function(req, res, next) {
-  res.render('entrar', { title: 'Express' });
-});
-router.get('/dadoscliente', cabideController.dadoscliente);
-
+router.get('/', cabideController.index);
+router.get('/ajuda', cabideController.ajuda);
+router.get('/colecoes', cabideController.colecoes);
+router.get('/contato', cabideController.contato);
+router.get('/entrar', cabideController.entrar);
+router.get('/login', cabideController.login);
+router.get('/modelos', cabideController.modelos);
+router.get('/pedidos', cabideController.pedidos);
+router.get('/quemSomos', cabideController.quemSomos);
+router.get('/sobre', cabideController.sobre);
+router.get('/busca', cabideController.busca);
 module.exports = router;
